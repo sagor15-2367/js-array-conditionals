668a59e6ae88dae8f4daccfbfc003fc73742cd6c
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 17:53:44 +0600

    ok

---
+++ array.js
@@ -0,0 +1,10 @@
+var friendsAge=[11, 21, 45, 17,14,105,6 ];
+var picnicFee=[5000,2000,200,150];
+var nayikas=['mahi','opu','biju','shanu'];
+var vowels=['a','e','i','o','u'];
+
+
+console.log(vowels);
+console.log(nayikas.length);
+console.log(picnicFee);
+console.log(friendsAge.length);
