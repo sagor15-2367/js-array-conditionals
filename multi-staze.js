351b93621926891eafe0b0dc663341bc9ea1430c
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 21:52:31 +0600

    ok

---
+++ multi-staze.js
@@ -0,0 +1,18 @@
+var money=30;
+var danishPrice=45;
+var butterBread=30;
+var tostBiscuit=20;
+
+
+if(danishPrice<money){
+    console.log('danis khamu');
+}
+else if(butterBread<=money){
+    console.log('butterban khamu');
+}
+else if(tostBiscuit<money){
+    console.log('tostbiskit khamu');
+}
+else{
+    console.log('cha khamu');
+}
