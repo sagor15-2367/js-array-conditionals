da3d38b9aa1c7759b57f60d984d5c83ce0b1a2b7
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 19:58:27 +0600

    ok

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers=[55,77,99,22,44];
+// use push to add element to an array as the last element array
+numbers.push(33);
+// console.log(numbers);
+var friends=['sagor','ratan','rumon'];
+friends.push('biju');
+friends.push('My Heart');
+console.log(friends);
+// use pop to get last element
+var element= friends.pop();
+friends.pop();
+console.log(friends);
+console.log(element);
+var kaku= numbers.pop(2);
+console.log(kaku);
