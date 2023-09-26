e4f9813a274009a9a78b58e61f08d14ac04d365c
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 17:39:51 +0600

    ok

---
+++ variable.js
@@ -0,0 +1,24 @@
+// number type variable
+var sunglassPrice=120;
+// string type variable
+var specialOneName="Sagor Alam";
+// boolean type variable
+var isSingle=true;
+// two type of number : integer:120,
+// float,decimal:3.61
+
+// variable naming convention : rules,best practice
+/**
+ * 1.single word
+ * 2.no gap or speace
+ * 3.no quote
+ * 4.no keyword
+ * 5.can not start with a number
+ * 6.can not contain dash
+ * 7.should use camelCase
+ */
+// math operations:+,-,*,/,%
+// shorthand: +=.-= ,++,--
+// convert : parseInt,parseFloat
+// toFixed
+// remainder: vagses
