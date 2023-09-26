d3d64b002ed72dfe173e31017e4348b8362cf0de
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 21:27:19 +0600

    ok

---
+++ conditional.js
@@ -0,0 +1,24 @@
+var iphonePrice = 10000;
+var myCash=180000;
+
+
+if(iphonePrice<myCash){
+    console.log('iphone nibo');
+}
+
+var chickenPrice=500;
+var myMoney=50;
+
+if(chickenPrice>=myMoney){
+    console.log('Give me a murgi');
+}
+if(chickenPrice<myMoney){
+    console.log('smashed potato and lentils soup');
+}
+
+if(chickenPrice<myMoney){
+    console.log('I will eat chicken')
+}
+else{
+    console.log('No eating');
+}
