6ef511c658bbbb5cde7c74ac7235dd20ebe85be1
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 22:10:42 +0600

    ok

---
+++ nested-condition.js
@@ -0,0 +1,17 @@
+var math=true;
+var geometry=true;
+var straightlint=false;
+
+if(math==true){
+    if(geometry==true){
+        if(straightlint==true){
+
+        }
+        else{
+            console.log('baka pothe cholba na')
+        }
+    }
+    else{
+        console.log('pithagorus hote parba na');
+    }
+}
