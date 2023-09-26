c0e72db6fa6bf8c82b09f86e905f4aafb4b1a0a4
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 22:39:03 +0600

    ok

---
+++ summary.js
@@ -0,0 +1,24 @@
+var destinations=['sajak','coxbazar','napal','Rangpur','dhaka'];
+
+console.log(destinations[2]);
+destinations[2]='thailand';
+var index = destinations.indexOf('balo');
+
+destinations.push('bhutan');
+destinations,pop()
+
+// conditionals
+// <,>,==,!=,<=,>=,
+// &&,||
+if(destinations[1]=='sajak'){
+
+}
+else if(destinations[2]=='bali'){
+
+}
+else if(destinations,length!==3){
+
+}
+else{
+    
+}
